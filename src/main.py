db0091b24cba1a48c5ed82d01708cac8cf6af1c5
imindMan <khanhdonglethanhtong7b2@gmail.com>
2022-03-12 14:18:38 +0700

    Run the project by running this file

---
+++ src/main.py
@@ -0,0 +1,17 @@
+import ClockContainer
+from tkinter import *
+
+
+def show_home(event):
+    screen.show_frame("Home")
+
+
+screen = ClockContainer.Clock()
+icon = PhotoImage(file=".\\data\\icon.png")
+screen.iconphoto(False, icon)
+screen.title("Pocket Clock")
+screen.resizable(False, False)
+screen.geometry("600x500")
+
+screen.bind("<Control-h>", show_home)
+screen.mainloop()
