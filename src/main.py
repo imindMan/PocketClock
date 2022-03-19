dc4f7e477b906106f41ee12247b5cc5792ce88f9
imindMan <khanhdonglethanhtong7b2@gmail.com>
2022-03-19 21:59:51 +0700

    Main.py : run project here

--- src/main.py
+++ src/main.py
@@ -8,6 +8,10 @@ def show_home(event):
     screen.show_frame("Home")
 
 
+def show_pomodoro(event):
+    screen.show_frame("Pomodoro")
+
+
 # set up the screen
 screen = ClockContainer.Clock()
 icon = PhotoImage(
@@ -18,4 +22,5 @@ screen.resizable(False, False)
 screen.geometry("600x500+400+150")
 
 screen.bind("<Control-h>", show_home)
+screen.bind("<Control-P>", show_pomodoro)
 screen.mainloop()
