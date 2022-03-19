957a2fe3ceee310a9e433851ec0780b3ac642386
imindMan <khanhdonglethanhtong7b2@gmail.com>
2022-03-19 21:27:08 +0700

    Running this main.py

--- src/main.py
+++ src/main.py
@@ -10,7 +10,8 @@ def show_home(event):
 
 # set up the screen
 screen = ClockContainer.Clock()
-icon = PhotoImage(file=".\\data\\icon.png")
+icon = PhotoImage(
+    file="H:\\K.DONG\\My book\\Learn to code\\Pocket Clock\\data\\icon.png")
 screen.iconphoto(False, icon)
 screen.title("Pocket Clock")
 screen.resizable(False, False)
