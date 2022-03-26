99280469a3b479dd6c8aa0cf8187ae1480106375
imindMan <khanhdonglethanhtong7b2@gmail.com>
2022-03-26 13:53:28 +0700

    Pomodoro clock

--- src/Pomodoro.py
+++ src/Pomodoro.py
@@ -4,3 +4,23 @@ import tkinter as tk
 class Pomodoro(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+        status = tk.Frame(master=self, width=123, height=500, bg="black")
+        status.pack(fill=tk.Y, side=tk.LEFT)
+
+        countdown_button = tk.Button(
+            master=status, text="Countdown", font=("Consolas", 16), bg="black", fg="white", command=lambda: controller.show_frame("Countdown"), border=0)
+        countdown_button.place(x=0, y=0)
+
+        finish_button = tk.Label(master=status, text="Finish", font=(
+            "Consolas", 18), bg="black", fg="white", height=0, width=10, command=controller.show_frame("Finish"))
+        finish_button.place(x=-1, y=34)
+
+        stopwatch_button = tk.Button(master=status, text="Stopwatch", font=(
+            "Consolas", 16), bg="black", fg="white", border=0, width=10, height=0, command=lambda: controller.show_frame("Stopwatch"))
+        stopwatch_button.place(x=0, y=75)
+        pomodoro_label = tk.Button(master=status, text="Pomodoro", font=("Consolas", 16), bg="white", fg="black", border=0, width=10, height=0)
+        pomodoro_label.place(x=0, y=109)
+        # main part
+        interface = tk.Frame(self,  bg="white", width=controller.width - 123)
+        interface.pack(side=tk.RIGHT, fill=tk.Y)
+        
