2a7e571bcbad8884e7875482ab22d0dcd81eff1d
imindMan <khanhdonglethanhtong7b2@gmail.com>
2022-03-26 16:41:57 +0700

    Pomodoro clock

--- src/Pomodoro.py
+++ src/Pomodoro.py
@@ -1,18 +1,20 @@
 import tkinter as tk
-
-
 class Pomodoro(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         status = tk.Frame(master=self, width=123, height=500, bg="black")
         status.pack(fill=tk.Y, side=tk.LEFT)
-
+        
+        def start():
+            pass
+        def stop():
+            pass
         countdown_button = tk.Button(
             master=status, text="Countdown", font=("Consolas", 16), bg="black", fg="white", command=lambda: controller.show_frame("Countdown"), border=0)
         countdown_button.place(x=0, y=0)
 
-        finish_button = tk.Label(master=status, text="Finish", font=(
-            "Consolas", 18), bg="black", fg="white", height=0, width=10, command=controller.show_frame("Finish"))
+        finish_button = tk.Button(master=status, text="Finish", font=(
+            "Consolas", 18), bg="black", fg="white", height=0, width=10, command = lambda : controller.show_frame("Finish"))
         finish_button.place(x=-1, y=34)
 
         stopwatch_button = tk.Button(master=status, text="Stopwatch", font=(
@@ -23,4 +25,15 @@ class Pomodoro(tk.Frame):
         # main part
         interface = tk.Frame(self,  bg="white", width=controller.width - 123)
         interface.pack(side=tk.RIGHT, fill=tk.Y)
+
+        time = "00:00:00"
+        time_label = tk.Label(interface, bg="white", fg="black", text=time, font=('Consolas', 30))
+        time_label.place(x=145, y=60)
         
+        start_button = tk.Button(
+            interface, text="Start", font=("Consolas", 20))
+        start_button.place(x=100, y=200)
+
+        stop_button = tk.Button(
+            interface, text="Stop", font=("Consolas", 20))
+        stop_button.place(x=270, y=200)
