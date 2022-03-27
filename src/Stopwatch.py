0bf5d4ed286bd02fc6e8e4ac9d2e45f6421fea0a
imindMan <khanhdonglethanhtong7b2@gmail.com>
2022-03-27 11:03:07 +0700

    Stopwatch page : a stopwatch clock

--- src/Stopwatch.py
+++ src/Stopwatch.py
@@ -41,17 +41,17 @@ class Stopwatch(tk.Frame):
         status = tk.Frame(master=self, width=123, height=500, bg="black")
         status.pack(side=tk.LEFT, fill=tk.Y)
 
-        countdown_label = tk.Button(
+        countdown_button = tk.Button(
             master=status, text="Countdown", font=("Consolas", 16), bg="black", fg="white", border=0, command=lambda: controller.show_frame("Countdown"))
-        countdown_label.place(x=0, y=0)
+        countdown_button.place(x=0, y=0)
 
         finish_button = tk.Button(master=status, text="Finish", font=(
             "Consolas", 18), bg="black", fg="white", height=0, width=10, border=0, command=lambda: controller.show_frame("Finish"))
         finish_button.place(x=-1, y=34)
 
-        stopwatch_button = tk.Label(master=status, text="Stopwatch", font=(
+        stopwatch_label = tk.Label(master=status, text="Stopwatch", font=(
             "Consolas", 18), bg="white", fg="black", border=0, width=10, height=0)
-        stopwatch_button.place(x=-1, y=75)
+        stopwatch_label.place(x=-1, y=75)
         # main section
         interface = tk.Frame(self,  bg="white", width=controller.width - 123)
         interface.pack(side=tk.RIGHT, fill=tk.Y)
