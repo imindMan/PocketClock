dd0e94d84727e2f499f053f43cf4653c5308d895
imindMan <100201613+imindMan@users.noreply.github.com>
2022-03-31 19:09:37 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -15,12 +15,12 @@ def show_pomodoro(event):
 # set up the screen
 screen = ClockContainer.Clock()
 icon = PhotoImage(
-    file="H:\\K.DONG\\My book\\Learn to code\\Pocket Clock\\data\\icon.png")
+    file=".\\data\\icon.png")
 screen.iconphoto(False, icon)
 screen.title("Pocket Clock")
 screen.resizable(False, False)
 screen.geometry("600x500+400+150")
 
-screen.bind("<Control-h>", show_home)
-screen.bind("<Control-P>", show_pomodoro)
+screen.bind("<Control-h>", show_home) # change the <Control-h> to the hot key you want, this hot key will return to the home page
+screen.bind("<Control-P>", show_pomodoro)  # change the <Control-p> to the hot key you want, this hot key will return to the pomodoro page
 screen.mainloop()
