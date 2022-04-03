966adf6bd4f70048fd257b522d7789513e3231cd
imindMan <khanhdonglethanhtong7b2@gmail.com>
2022-04-03 07:18:21 +0700

    To do list: first commit

---
+++ src/ToDoList.py
@@ -0,0 +1,21 @@
+import tkinter as tk 
+class ToDoList(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        # status
+        status = tk.Frame(master=self, width=123, bg="black")
+        status.pack(fill=tk.Y, side=tk.LEFT)
+        # countdown status
+        countdown_label = tk.Label(
+            master=status, text="Countdown", font=("Consolas", 18), bg="white", fg="black")
+        countdown_label.place(x=0, y=0)
+        # finish status
+        finish_button = tk.Button(master=status, text="Finish", font=(
+            "Consolas", 18), bg="black", fg="white", height=0, width=10, border=0, command=lambda: controller.show_frame("Finish"))
+        finish_button.place(x=-1, y=34)
+        # stopwatch status
+        stopwatch_button = tk.Button(master=status, text="Stopwatch", font=(
+            "Consolas", 16), bg="black", fg="white", border=0, width=10, height=0, command=lambda: controller.show_frame("Stopwatch"))
+        stopwatch_button.place(x=-1, y=75)
+        todo_label = tk.Button(master=status, text="ToDoList", font=("Consolas", 16), bg="white", fg="black", border=0, width=10, height=0)
+        todo_label.place(x=0, y=109)
