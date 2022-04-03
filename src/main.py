9fcefa346aa6efd3db80590067a88632794f8041
imindMan <khanhdonglethanhtong7b2@gmail.com>
2022-04-03 07:17:53 +0700

    Run project here

--- src/main.py
+++ src/main.py
@@ -11,6 +11,8 @@ def show_home(event):
 def show_pomodoro(event):
     screen.show_frame("Pomodoro")
 
+def show_to_do_list(event):
+    screen.show_frame("ToDoList")
 
 # set up the screen
 screen = ClockContainer.Clock()
@@ -25,4 +27,5 @@ screen.geometry("600x500+400+150")
 screen.bind("<Control-h>", show_home)
 # change the <Control-p> to the hot key you want, this hot key will return to the pomodoro page (if you aren't using Windows)
 screen.bind("<Control-P>", show_pomodoro)
+screen.bind("<Shift-Tab>", show_to_do_list)
 screen.mainloop()
