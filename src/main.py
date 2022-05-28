39337cacefa6e3f825e23978b2585c0f977e6b7c
imindMan <khanhdonglethanhtong7b2@gmail.com>
2022-05-28 21:48:17 +0700

    Fix the icon path error

--- src/main.py
+++ src/main.py
@@ -11,14 +11,14 @@ def show_home(event):
 def show_pomodoro(event):
     screen.show_frame("Pomodoro")
 
+
 def show_to_do_list(event):
     screen.show_frame("ToDoList")
 
+
 # set up the screen
 screen = ClockContainer.Clock()
-icon = PhotoImage(
-    file="H:\\K.DONG\\My book\\Learn to code\\Pocket Clock\\data\\icon.png")  # change this path to your path to the icon.png
-screen.iconphoto(False, icon)
+screen.iconbitmap(".\\data\\icon.ico")
 screen.title("Pocket Clock")
 screen.resizable(False, False)
 screen.geometry("600x500+400+150")
@@ -27,5 +27,5 @@ screen.geometry("600x500+400+150")
 screen.bind("<Control-h>", show_home)
 # change the <Control-p> to the keyboard shortcut you want, this keyboard shortcut will return to the pomodoro page (if you aren't using Windows)
 screen.bind("<Control-P>", show_pomodoro)
-screen.bind("<Shift-Tab>", show_to_do_list) # if you like, you can change it
+screen.bind("<Shift-Tab>", show_to_do_list)  # if you like, you can change it
 screen.mainloop()
